466c4a58a05cb92ce2eaa286db1c35c709d64091
Ethan L <ethanlogue03@gmail.com>
2023-11-30 01:17:23 -0500

    newly visible button opens event in new tab

--- mycourses/mycourses-utils.js
+++ mycourses/mycourses-utils.js
@@ -3,7 +3,7 @@
 // @namespace    Monkey Scripts
 // @description  Quality of life improvements for RIT MyCourses
 // @author       Ethan Logue
-// @version      1.0
+// @version      1.1
 // @match        https://mycourses.rit.edu/*
 // @icon         https://mycourses.rit.edu/d2l/lp/web/faviconView?variant=0&version=1
 // @grant        none
@@ -12,8 +12,28 @@
 (function() {
     'use strict';
 
+    // custom css rules
     var style = document.createElement('style');
     style.innerHTML = `
+        /* container holding the original content */
+        .d2l-datalist-item-content > div {
+            padding-top: 8px !important;
+        }
+
+        /* link tag holding the span */
+        .d2l-datalist-item-actioncontrol {
+            padding: 0 10px !important;
+            margin-left: 1rem !important;
+            color: var(--d2l-color-celestine) !important;
+        }
+
+        /* hover state of the link tag */
+        .d2l-datalist-item-actioncontrol:hover {
+            color: var(--d2l-color-celestine-minus-1) !important;
+            text-decoration: underline !important;
+        }
+
+        /* span tag withing the link thats usually hidden */
         .d2l-offscreen {
             position: relative !important;
             left: 0 !important;
@@ -21,4 +41,31 @@
     `;
 
     document.head.appendChild(style);
+
+    // sidebar events seem to load asynchronously so this gives it a second to load 
+    // and tries 10 times to ensure slow connections can still be handled
+    window.onload = function() {
+        var maxAttempts = 10;
+        var attempts = 0;
+        var intervalTime = 1000;
+
+        var interval = setInterval(function() {
+            attempts++;
+            console.log('Checking for element: ' + attempts);
+
+            // gets the span tag that holds the text for the event
+            var viewEvents = document.querySelectorAll('span.d2l-offscreen');
+            if (viewEvents.length > 0) {
+                clearInterval(interval); // clear the interval once the element is found (currently doesn't clear it for some reason)
+                viewEvents.forEach(function(event) {
+                    event.textContent = 'Open in New Tab';
+                    event.parentElement.target = '_blank';
+                });
+            } else if (attempts >= maxAttempts) {
+                console.log('Element not found after ' + attempts + ' attempts.');
+                clearInterval(interval); // stops checking when maximum attempts reached
+            }
+        }, intervalTime);
+    }
+
 })();
