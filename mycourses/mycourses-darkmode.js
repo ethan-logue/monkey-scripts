485d5b5472dc3efc25ed4f2c240f6df824dd2134
Ethan L <ethanlogue03@gmail.com>
2024-05-02 23:01:54 -0400

    started styling, but lots of elements under shadowroot

--- mycourses/mycourses-darkmode.js
+++ mycourses/mycourses-darkmode.js
@@ -12,5 +12,38 @@
 (function() {
     'use strict';
 
-    // Your code here...
+    // custom css rules
+    var style = document.createElement('style');
+    style.innerHTML = `
+        /* background color of the page */
+        .d2l-tiles-container {
+            background-color: #121212 !important;
+        }
+
+        /* background color of the navigation bar */
+        .d2l-navigation-s * {
+            background-color: #242424 !important;
+            color: #eeeeee !important;
+        }
+
+        .d2l-navigation-s img {
+            filter: invert(1);
+        }
+
+        /* background color of the tiles */
+        .d2l-tile {
+            background-color: #242424 !important;
+        }
+
+        /* text color */
+        .d2l-typography {
+            color: #eeeeee !important;
+        }
+
+        .d2l-tile.d2l-widget-padding-full > .d2l-widget-content * {
+            background-color: #242424 !important;
+        }
+    `;
+
+    document.head.appendChild(style);
 })();
