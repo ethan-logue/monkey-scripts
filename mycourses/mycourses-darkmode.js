3b4f12175e718e6b4ec810d112c7feb745b2eafa
Ethan L <ethanlogue03@gmail.com>
2024-05-02 22:00:27 -0400

    initial commit of darkmode script

---
+++ mycourses/mycourses-darkmode.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         mycourses-darkmode
+// @namespace    Monkey Scripts
+// @description  Dark mode comes to MyCourses!
+// @author       Ethan Logue
+// @version      1.0
+// @match        https://mycourses.rit.edu/d2l/*
+// @icon         https://mycourses.rit.edu/d2l/lp/web/faviconView?variant=0&version=1
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
